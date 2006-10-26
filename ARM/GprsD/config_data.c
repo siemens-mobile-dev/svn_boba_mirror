2bf804fc69b58c94e533b45dea42993f89d9f29b
Kibab <Kibab@98664855-a6bc-5d49-be9f-2936a658fbf1>
2006-10-26 11:44:11 +0000

    [+] загрузка данных доступа из конфиг-файла
    git-svn-id: svn://svn.boba.su@209 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ ARM/GprsD/config_data.c
@@ -0,0 +1,13 @@
+#include "..\inc\swilib.h"
+#include "..\inc\cfg_items.h"
+__root const CFG_HDR cfghdr0={CFG_STR_WIN1251,"APN:",0,19};
+__root const char cfgAPN[20]="internet.mts.ru";
+
+__root const CFG_HDR cfghdr1={CFG_STR_WIN1251,"Username:",0,19};
+__root const char cfgUSER[20]="mts";
+
+__root const CFG_HDR cfghdr2={CFG_STR_WIN1251,"Password:",0,19};
+__root const char cfgPASS[20]="mts";
+
+__root const CFG_HDR cfghdr3={CFG_UINT,"Net timeout:",0,0xFFFFFFFF};
+__root const unsigned int cfgTimeout=0x5F5E0FF; // 99999999 секунд =)
