e2996428e2dddea4800b5640349812efd6598ade
_Rst7 <_Rst7@98664855-a6bc-5d49-be9f-2936a658fbf1>
2006-10-30 12:21:22 +0000

    Добавил освобождение памяти, занимаемого демоном при закрытии IDLE_CSM
    git-svn-id: svn://svn.boba.su@224 98664855-a6bc-5d49-be9f-2936a658fbf1

--- ARM/MegaDial/main.c
+++ ARM/MegaDial/main.c
@@ -10,6 +10,7 @@
 CSM_DESC icsmd;
 
 int (*old_icsm_onMessage)(CSM_RAM*,GBS_MSG*);
+void (*old_icsm_onClose)(CSM_RAM*);
 
 int (*old_ed_onkey)(GUI *gui, GUI_MSG *msg);
 void (*old_ed_ghook)(GUI *gui, int cmd);
@@ -597,12 +598,21 @@ int MyIDLECSM_onMessage(CSM_RAM* data,GBS_MSG* msg)
   return(csm_result);
 }
 
+void MyIDLECSM_onClose(CSM_RAM *data)
+{
+  extern void seqkill(void *data, void(*next_in_seq)(CSM_RAM *), void *data_to_kill, void *seqkiller);
+  extern void *ELF_BEGIN;
+  seqkill(data,old_icsm_onClose,&ELF_BEGIN,SEQKILLER_ADR());
+}
+
 int main(void)
 {
   LockSched();
   CSM_RAM *icsm=FindCSMbyID(CSM_root()->idle_id);
   memcpy(&icsmd,icsm->constr,sizeof(icsmd));
   old_icsm_onMessage=icsmd.onMessage;
+  old_icsm_onClose=icsmd.onClose;
+  icsmd.onClose=MyIDLECSM_onClose;
   icsmd.onMessage=MyIDLECSM_onMessage;
   icsm->constr=&icsmd;
   UnlockSched();
