b9695ee43fe736265dba97328ff1a1e02a7780b4
_Rst7 <_Rst7@98664855-a6bc-5d49-be9f-2936a658fbf1>
2006-11-11 08:03:14 +0000

    Моднячий CpuMon с настройками. +Заточил под эльфлоадер 1.8 +Теперь перезапускает систему подсчета загрузки проца, не затыкается на 100%
    git-svn-id: svn://svn.boba.su@247 98664855-a6bc-5d49-be9f-2936a658fbf1

--- ARM/cpumon/main.c
+++ ARM/cpumon/main.c
@@ -1,12 +1,45 @@
 #include "..\inc\swilib.h"
+#include "conf_loader.h"
 
-#define UPDATE_TIME (1*262)
-#define WIDTH 50
-#define HIGHT 20
-#define X 131-50
-#define Y 132
+#define idlegui_id (((int *)data)[DISPLACE_OF_IDLEGUI_ID/4])
 
-#define RGB8(R,G,B) (B+(G<<2)+(R<<5))
+#define MIN_WIDTH 30
+#define MIN_HEIGHT 10
+#define MIN_UPTIME 1
+#define START_DELAY (20 * 216)
+
+#define CLOCK_26     26
+#define CLOCK_52     52
+#define CLOCK_104    104
+
+#define RGB16(R, G, B) ((B & 0x1F) | ((G & 0x3F) << 5) | ((R & 0x1F) << 11))
+
+extern const unsigned int cfgX;
+extern const unsigned int cfgY;
+
+extern const unsigned int cfgW;
+extern const unsigned int cfgH;
+
+extern const int cfgShowIn;
+extern const int cfgStTxt;
+extern const unsigned int cfgUpTime;
+
+extern const unsigned int cfgLoadR52;
+extern const unsigned int cfgLoadG52;
+extern const unsigned int cfgLoadB52;
+
+extern const unsigned int cfgLoadR104;
+extern const unsigned int cfgLoadG104;
+extern const unsigned int cfgLoadB104;
+
+unsigned int uiUpdateTime, uiWidth, uiHeight;
+
+unsigned short* img1_bmp;
+
+IMGHDR img1;
+
+unsigned char* loads;
+unsigned char* clocks;
 
 CSM_DESC icsmd;
 
@@ -20,84 +53,106 @@ WSHDR *ws1;
 unsigned char hhh;
 char cop;
 
-unsigned char img1_bmp[WIDTH*HIGHT-1];
-
-const IMGHDR img1=
-{
-  WIDTH,
-  HIGHT,
-  5,
-  0,
-  (char *)img1_bmp
-};
-
-unsigned char loads[WIDTH-1];
-unsigned char clocks[WIDTH-1];
-
 DrwImg(IMGHDR *img, int x, int y, int *pen, int *brush)
 {
   RECT rc;
   DRWOBJ drwobj;
-  StoreXYWHtoRECT(&rc,x,y,img->w,img->h);
-  SetPropTo_Obj5(&drwobj,&rc,0,img);
-  SetColor(&drwobj,pen,brush);
+  StoreXYWHtoRECT(&rc, x, y, img->w, img->h);
+  SetPropTo_Obj5(&drwobj, &rc, 0, img);
+  SetColor(&drwobj, pen, brush);
   DrawObject(&drwobj);
 }
 
 void TimerProc(void)
 {
-  GBS_SendMessage(MMI_CEPID,0xDEAD,GetCPULoad());
-  GBS_StartTimerProc(&mytmr,UPDATE_TIME,TimerProc);
+  if (!getCpuUsedTime_if_ena())
+  {
+    StartCpuUsageCount();
+  }
+  GBS_SendMessage(MMI_CEPID, 0xDEAD, GetCPULoad());
+  GBS_StartTimerProc(&mytmr, (uiUpdateTime > MIN_UPTIME) ? uiUpdateTime : MIN_UPTIME, TimerProc);
 }
 
+
+unsigned int ClocksColour(unsigned int clock)
+{
+  switch (clock)
+  {
+  case CLOCK_26:
+    return (/*RGB16(cfgLoadR, cfgLoadG, cfgLoadB)*/ 0xE000);
+  case CLOCK_52:
+    return (RGB16(cfgLoadR52, cfgLoadG52, cfgLoadB52));
+  case CLOCK_104:
+    return (RGB16(cfgLoadR104, cfgLoadG104, cfgLoadB104));
+  default:
+    return (/*RGB16(0, 0, 0)*/ 0xFFFF);
+  }
+}
 
 int MyIDLECSM_onMessage(CSM_RAM* data,GBS_MSG* msg)
 {
   int csm_result;
-  unsigned int x;
-#define idlegui_id (((int *)data)[DISPLACE_OF_IDLEGUI_ID/4])
+  unsigned char fShow;
 
-  //Накапливаем значения
-  if (msg->msg==0xDEAD)
+	//Накапливаем значения
+  if (msg->msg == 0xDEAD)
   {
     LockSched();
-    loads[hhh]=HIGHT*/*GetCPULoad()*/msg->submess/100;
-    clocks[hhh]=GetCPUClock()/14;
+    loads[hhh]  = uiHeight * msg->submess / 100;
+    clocks[hhh] = GetCPUClock()/* / 26*/;
     UnlockSched();
     hhh++;
-    if (hhh>=WIDTH) {hhh=0; cop=1;}
-    csm_result=0;
+    if (hhh >= uiWidth)
+    {
+      hhh = 0;
+      cop = 1;
+    }
+    csm_result = 0;
   }
   else
-    csm_result=old_icsm_onMessage(data,msg); //Вызываем старый обработчик событий
-  if (IsGuiOnTop(idlegui_id)/*&&IsUnlocked()*/) //Если IdleGui на самом верху
+    csm_result = old_icsm_onMessage(data, msg); //Вызываем старый обработчик событий
+
+  switch (cfgShowIn)
   {
-    GUI *igui=GetTopGUI();
+  case 0:
+    fShow = !IsUnlocked();
+    break;
+  case 1:
+    fShow = IsUnlocked();
+    break;
+  case 3:
+    fShow = 1;
+  }
+
+  if (IsGuiOnTop(idlegui_id) && fShow) //Если IdleGui на самом верху
+  {
+    GUI *igui = GetTopGUI();
     if (igui) //И он существует
     {
-      void *idata=GetDataOfItemByID(igui,2);
+      void *idata = GetDataOfItemByID(igui, 2);
       if (idata)
       {
-	void *canvasdata=((void **)idata)[DISPLACE_OF_IDLECANVAS/4];
-	int y;
-	DrawCanvas(canvasdata,X,Y,X+WIDTH,Y+HIGHT,1);
-	//рисуем нашу требуху
-	//     zeromem(img1_bmp,sizeof(img1_bmp));
-	int h=hhh;
-	for(x=0;x<WIDTH;x++){
-	  for(y=0;y<HIGHT;y++){
-	    if (y<loads[h]){
-	      img1_bmp[x+(HIGHT-1-y)*WIDTH]=RGB8(clocks[h],0,0);
-	    }else{
-	      img1_bmp[x+(HIGHT-1-y)*WIDTH]=RGB8(6,0,0);
-	    }
-	  }
-	  h++;
-	  if (h>=WIDTH) h=0;
-	}
-	DrwImg((IMGHDR *)&img1,X,Y,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(1));
-	if (cop==0)
-	  DrawString(ws1,0,100,131,131,11,0x20,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(1));
+        void *canvasdata = ((void **)idata)[DISPLACE_OF_IDLECANVAS / 4];
+        DrawCanvas(canvasdata, cfgX, cfgY, cfgX + uiWidth, cfgY + uiHeight, 1);
+        //рисуем нашу требуху
+        int h = hhh;
+        for(unsigned int x = 0; x < uiWidth; x++)
+        {
+          for(unsigned int y = 0; y < uiHeight; y++)
+          {
+            if (y < loads[h])
+              img1_bmp[x + (uiHeight - 1 - y) * uiWidth] = ClocksColour(clocks[h]);
+            else
+              img1_bmp[x + (uiHeight - 1 - y) * uiWidth] = 0xE000; //RGB16(21, 42, 21);
+          }
+          h++;
+          if (h >= uiWidth)
+            h = 0;
+        }
+        DrwImg((IMGHDR *)&img1, cfgX, cfgY, GetPaletteAdrByColorIndex(0), GetPaletteAdrByColorIndex(1));
+        if (!cop && cfgStTxt)
+          DrawString(ws1, 0, cfgY-14, ScreenW()-1, ScreenH()-1, SMALL_FONT, 0x20,
+                     GetPaletteAdrByColorIndex(0), GetPaletteAdrByColorIndex(1));
       }
     }
   }
@@ -116,6 +171,29 @@ void MyIDLECSM_onClose(CSM_RAM *data)
 int main(void)
 {
   hhh=0;
+  InitConfig();
+
+  //========================
+  uiUpdateTime = (262 * cfgUpTime) / 10;
+  uiWidth  = (cfgW < MIN_WIDTH)  ? MIN_WIDTH  : cfgW;
+  uiHeight = (cfgH < MIN_HEIGHT) ? MIN_HEIGHT : cfgH;
+
+  img1_bmp = malloc(2 * uiWidth * uiHeight - 1);
+  zeromem(img1_bmp, 2 * uiWidth * uiHeight - 1);
+
+  img1.w = uiWidth;
+  img1.h = uiHeight;
+  img1.bpnum = 8;
+  img1.zero = 0;
+  img1.bitmap = (char*) img1_bmp;
+
+  loads = malloc(uiWidth - 1);
+  zeromem(loads, uiWidth - 1);
+
+  clocks = malloc(uiWidth - 1);
+  zeromem(clocks, uiWidth - 1);
+  //========================
+
   LockSched();
   CSM_RAM *icsm=FindCSMbyID(CSM_root()->idle_id);
   memcpy(&icsmd,icsm->constr,sizeof(icsmd));
@@ -126,7 +204,7 @@ int main(void)
   icsm->constr=&icsmd;
   ws1=AllocWS(100);
   wsprintf(ws1,"%t","CPUMon by BoBa, Rst7");
-  GBS_StartTimerProc(&mytmr,UPDATE_TIME*10,TimerProc);
+  GBS_StartTimerProc(&mytmr,START_DELAY,TimerProc);
   UnlockSched();
   return 0;
 }
