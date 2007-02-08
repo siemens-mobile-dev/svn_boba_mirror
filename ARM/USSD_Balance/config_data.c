3ad6e39e54f339a25ad1661874f26aa7ade922cc
Captain_SISka <Captain_SISka@98664855-a6bc-5d49-be9f-2936a658fbf1>
2007-02-08 20:55:10 +0000

    git-svn-id: svn://svn.boba.su@452 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ ARM/USSD_Balance/config_data.c
@@ -0,0 +1,8 @@
+#include "..\inc\cfg_items.h"
+#include "..\inc\swilib.h"
+
+//Конфигурация
+__root const CFG_HDR cfghdr0={CFG_STR_WIN1251,"USSD string",0,63};
+__root const char ussd_str[64]="*100#";
+
+
