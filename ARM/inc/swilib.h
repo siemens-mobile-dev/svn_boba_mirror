86fee002460d14bab1fd2b07426ab892a7f1f0be
KreN <KreN@98664855-a6bc-5d49-be9f-2936a658fbf1>
2007-03-26 17:49:21 +0000

    поправил описания клавиш на сголд
    git-svn-id: svn://svn.boba.su@745 98664855-a6bc-5d49-be9f-2936a658fbf1

--- ARM/inc/swilib.h
+++ ARM/inc/swilib.h
@@ -385,8 +385,8 @@ typedef struct
 #define RIGHT_SOFT      0x04
 #define GREEN_BUTTON    0x0B
 #define RED_BUTTON      0x0C
-#define VOL_UP_BUTTON   0x14
-#define VOL_DOWN_BUTTON 0x15
+#define VOL_UP_BUTTON   0x0D
+#define VOL_DOWN_BUTTON 0x0E
 #define UP_BUTTON       0x3B
 #define DOWN_BUTTON     0x3C
 #define LEFT_BUTTON     0x3D
