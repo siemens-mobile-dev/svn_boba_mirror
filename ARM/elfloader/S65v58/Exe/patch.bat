0bd1f1f8a6e37a282d9af4493f881430cd9a74ef
Shadow <Shadow@98664855-a6bc-5d49-be9f-2936a658fbf1>
2007-08-18 19:21:40 +0000

    [+]Аналогично ревизии 1357
    git-svn-id: svn://svn.boba.su@1371 98664855-a6bc-5d49-be9f-2936a658fbf1

--- ARM/elfloader/S65v58/Exe/patch.bat
+++
@@ -1,2 +0,0 @@
-elf2vkp.exe ELFLoader.elf aaa.vkp C:\S65.bin
-copy /b swilib.vkp+swi.vkp+aaa.vkp ElfPack.vkp
--- ARM/elfloader/SL65v53/Exe/patch.bat
+++
@@ -1,2 +0,0 @@
-elf2vkp.exe ELFLoader.elf aaa.vkp sl65v53.bin
-copy /b swilib.vkp+swi.vkp+aaa.vkp ElfPack.vkp
