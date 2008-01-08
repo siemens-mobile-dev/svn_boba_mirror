766ba6ae6a7cfc18a2579daaec544ee211282d57
Hussein <Hussein@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-01-08 12:39:44 +0000

    HelloWorld - название говорит само за себя ;)
    git-svn-id: svn://svn.boba.su@1827 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/HelloWorld/main.c
@@ -0,0 +1,42 @@
+#include "..\\include\Lib_Clara.h"
+#include "..\\include\Dir.h"
+
+void elf_exit(void)
+
+{
+  kill_data(&ELF_BEGIN, (void(*)(void*))mfree_adr());
+}
+
+u16 timer; // ID таймера
+int lamp=0; // текуща€ €ркость лампы
+int n=10; // счетчик циклов мигани€
+
+void onTimer (u16 * unk , void * data)
+{
+  // если счетчик не дошел до нул€
+  if(n--)
+  {
+    // перезапускаем таймер
+    Timer_ReSet(&timer,500,onTimer,0);
+    // переключаем лампу
+    SetLampLevel(lamp^=0x10);
+  }
+  else
+  {
+    // убираем "заметки" на StandBy
+    StatusIndication_ShowNotes(0x6FFFFFFF);
+    // выключаем лампу
+    SetLampLevel(0);
+    // завершаем работу и выгружаем эльфа из пам€ти
+    SUBPROC(elf_exit);
+  }
+}
+
+int main (void)
+{
+  // выводим текст в "заметки" на StandBy
+  StatusIndication_ShowNotes(STR("Hello World!"));
+  // запускаем таймер на 0.5 сек.
+  timer=Timer_Set(500,onTimer,0);
+  return(0);
+}
