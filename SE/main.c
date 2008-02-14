6937d4767f73cfe6425d901c836b69ef77b4f174
gpch <gpch@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-02-14 15:55:07 +0000

    + System Temp ELF Source
    git-svn-id: svn://svn.boba.su@1901 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/main.c
@@ -0,0 +1,42 @@
+#include "..\\include\Lib_Clara.h"
+#include "..\\include\Dir.h"
+
+BATT  bat;
+u16   ws[193];
+u16   timer; // ID таймера
+
+void elf_exit(void)
+{
+  kill_data(&ELF_BEGIN, (void(*)(void*))mfree_adr());
+}
+ 
+void onTimer (u16 * unk , void * data)
+{
+  GetBattaryState((int const*)1 , &bat);
+  snwprintf(ws,196,(u16*)L"Temp = %d C",bat.SystemTemperature);
+  StatusIndication_ShowNotes(Str2ID(ws,0,SID_ANY_LEN));
+  Timer_ReSet(&timer,1000,onTimer,0);
+}
+
+int NewKey(int key, int r1 , int mode)
+{
+  if (key==KEY_POWER)
+  {
+    if(mode==KBD_SHORT_PRESS)
+      {
+        ModifyKeyHook(NewKey,0);
+        Timer_Kill(&timer);
+        StatusIndication_ShowNotes(0x6FFFFFFF);
+        SUBPROC(elf_exit);
+        return(0);
+      }   
+  }
+  return(0);
+}
+  
+int main(u16 *exename,u16 *filename)
+{
+  ModifyKeyHook(NewKey,1);
+  timer=Timer_Set(1000,onTimer,0);
+  return(0);
+}
