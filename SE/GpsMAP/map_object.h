cb22aa14f2f1a891eb7f7da1269cb187c14032b4
KreN <KreN@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-03-04 18:37:33 +0000

    Вот решил выложить пока ещё работает ) эльф GpsMAP умеет шариться по карте и если есть возможность переходить на текущее местоположение. Для использования необходима карта, порезанная на мелкие куски в формате пнг, имена файлов - %Town%_%y%_%x%.png и должны лежать в каталоге ELF_DIR/GpsMAP/%Town%/ где %y% значения от 0 до последнего порезанного по вертикали, %x% от 0 до последнего по горизонтали. %Town% - переменная с конфига. Конфиг файл GpsMAP.ini должен лежать в каталог DIR_ELFS_CONFIG и иметь следующий формат:
    [Town 0]   // Секция одного города
    Name = Rostov  // Название города, а потом каталога с пнг и самих пнг
    MapStartX=39°31.943'  // начальная точка по x на карте
    MapStartY=47°19.998'  // начальная точка по y на карте
    MapEndX=39°52.587'   // конечная точка по x на карте
    MapEndY=47°04.029'   // конечная точка по y на карте
    
    Для определения координат в каталоге %Town% должен лежать файл bs_dat.csv с форматом:
    1;2;39°43.094';47°13.362';ул. Б. Садовая, 69 (РГЭУ);?
    где первое значение номер бс (CID/10)
    второе - lac
    третье - координата по x
    четвертое - координата по y
    остальное до конца строки произвольно
    
    git-svn-id: svn://svn.boba.su@2011 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/GpsMAP/map_object.h
@@ -0,0 +1,24 @@
+#ifndef _MAP_OBJECT_H_
+  #define _MAP_OBJECT_H_
+
+#include "pngread.h"
+#include "read_ini.h"
+
+typedef struct {
+  char dummy[0x2c]; 
+} GUI_MAP;
+
+
+typedef struct {
+  DISP_OBJ dsp_obj;
+  int x1,x2,y1,y2;
+  int disp_x, disp_y;
+  int total_x_pos, total_y_pos;
+  MAIN_INI ini;
+  unsigned int *scr_buf;
+  LIST *png_cache;
+  float Xres, Yres;
+} DISP_OBJ_MAP;
+
+GUI_MAP *CreateMapGUI(BOOK *bk);
+#endif
