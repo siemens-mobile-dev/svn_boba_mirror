42dcdd5b2b5d58b3d1a6904c1502aabf3ae41c2f
jdp <jdp@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-03-08 09:15:58 +0000

    +target W810_R4DB005
    git-svn-id: svn://svn.boba.su@2025 98664855-a6bc-5d49-be9f-2936a658fbf1

--- SE/ElfPack/Target/W810_R4DB005.h
+++ SE/ElfPack/Target/W810_R4DB005.h
@@ -3,18 +3,19 @@
 #else
 #ifndef _W810_R4DB005_
 #define _W810_R4DB005_
-//#define pKBD 0x4C057C48
+
 #define EXT_TABLE 0x444E9A98
+#define RUN_CHECK 0x455B5945
 
 #define STR_START 0x0B9D
 #define STR_APPLICATION 0x0342
-#define STR_Saved_in_Applications_on_Memory_Stick 0x0FEF
-#define STR_Saved_in_Applications_in_phone_memory 0x0FEE
+#define STR_Saved_on_Memory_Stick 0x0FEF
+#define STR_Saved_in_phone_memory 0x0FEE
 
 #define ELF_SMALL_ICON 0xE5AC
 
-#define ROOT_APP 0x4C2EE860
-#define PID_MMI 0x100E5							//
+//#define ROOT_APP 0x4C2EE860
+//#define PID_MMI 0x100E5							//
 #define HELPER_SIG 0x6FF0
 //#define RAM_BASE (0x4C03F018)
 
@@ -29,6 +30,8 @@
 #define PATH_OTHER_INT 0x44647E08
 #define PATH_OTHER_EXT 0x44646828
 
+#define PATH_ELF_ROOT_INT		(L"/usb/other/ZBin")
+#define PATH_ELF_ROOT_EXT		(L"/card/MSSEMC/Media files/other/ZBin")
 #define PATH_ELF_INT		(L"/usb/other/ZBin")
 #define PATH_ELF_EXT		(L"/card/MSSEMC/Media files/other/ZBin")
 #define PATH_ELF_DAEMONS_INT 	(L"/usb/other/ZBin/Daemons")
@@ -38,17 +41,12 @@
 #define	PATH_INI		(L"/usb/other/ini")
 #define	PATH_DEFAULT		(L"/usb/other")
 
-
-#define DB_CMD_SETSMALLICON 0x36					
-#define DB_DB_EXT_C1 0x290						
-
-
-//Webpage_Run_Page
 #define ELF_RUN_PAGE_PREVIOUS_EVENT  0x455B27DD
 #define ELF_RUN_PAGE_ACCEPT_EVENT    0x455B27DD
 #define ELF_RUN_PAGE_CANCEL_EVENT    0x455B27E5
 #define ELF_RUN_PAGE_PAGE_EXIT_EVENT 0x455B2879
-//ext_mp3
-#define RUN_CHECK 0x455B5945
+
+#define DB_CMD_SETSMALLICON 0x36					
+#define DB_DB_EXT_C1 0x290	
 #endif
 #endif
