37e681c0545ffbaf2ff94ee84555b948d919dc63
Hussein <Hussein@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-04-08 12:38:16 +0000

    +Обдиратель экрана
    git-svn-id: svn://svn.boba.su@2109 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/SShoot/main.c
@@ -0,0 +1,263 @@
+#include "..\\include\Lib_Clara.h"
+#include "..\\include\Dir.h"
+
+#define PLATFORM_DB2010 8
+#define PLATFORM_DB2020 9
+
+#pragma pack(1)
+typedef struct
+{
+  u16 ID;
+  int size;
+  u16 zero;
+  u16 zero1;
+  int dataoffset;
+  int sizeofheader;
+  int width;
+  int height;
+  u16 planes;
+  u16 bpp;
+  int compression;
+  int sizeofimage;
+  int hresolution;
+  int vresolution;
+  int colors;
+  int important;
+}BMP_HDR;
+#pragma pack()
+
+int xsize;
+int ysize;
+int KeyActiv=0x20;
+int KeyPressMode=0x03;
+
+BMP_HDR *hdr;
+u16 * path=0;
+
+BOOK * SSBook;
+
+int TerminateElf(void * ,BOOK* book)
+{
+  FreeBook(book);
+  return(1);
+}
+
+typedef struct
+{
+  BOOK * book;
+}MSG;
+
+int ShowAuthorInfo(void *mess ,BOOK* book)
+{
+  MSG * msg = (MSG*)mess;
+  MessageBox(0x6fFFFFFF,STR("ScreenShooter v1.2\n\n(c)2007-2008 Hussein"),0, 1 ,5000,msg->book);
+  return(1);
+}
+
+const PAGE_MSG SS_PageEvents[]@ "DYN_PAGE" ={
+  ELF_TERMINATE_EVENT , TerminateElf,
+  ELF_SHOW_INFO_EVENT  , ShowAuthorInfo,
+  0,0
+};
+
+PAGE_DESC base_page ={"SS_BasePage",0,SS_PageEvents};
+
+
+
+void elf_exit(void)
+{
+  kill_data(&ELF_BEGIN, (void(*)(void*))mfree_adr());
+}
+
+int h2i(char * h)
+{
+  char c;
+  int res=0;
+  while((*h++)!='x');
+  do
+  {
+    c=*h++;
+    if (c>0x60) c-=0x20; else c-=0x30;
+    if (c>9) c-=7;
+    res<<=4;
+    res|=c;
+  }
+  while(*h!=';');
+  return(res);
+}
+
+void Snap(void)
+{
+  int f;
+  int x;
+  int y;
+  u16 cl=0;
+  char *bmp =new char[((xsize*ysize*3)+54)];
+  char *bp=bmp+54;
+  u16 * fname =new u16[50];
+  u16 * fpath =new u16[wstrlen(path)+20];
+  DATETIME dt;
+  int platform = GetChipID()>>12;
+  char * BUFF_p;
+  switch (platform)
+  {
+  case PLATFORM_DB2010:
+    {
+      BUFF_p=(char*)(**(int**)get_VBUFFER());
+    }
+    break;
+  case PLATFORM_DB2020:
+    {
+      SURFACE * mx =(SURFACE*)(*get_Surfaces());
+      while ((mx->RefCount!=1)&&(!mx->Display)) mx=mx->next;
+      BUFF_p=(char*)mx->Buffer;
+    }
+    break;
+  default:
+    return;
+  }
+
+  memcpy(bmp,hdr,54);
+  y=ysize;
+  do
+  {
+
+    char * p=BUFF_p+(2*xsize*(y-1));
+    x=0;
+    do
+    {
+      if (platform==PLATFORM_DB2020)
+      {
+        cl=p[0]+(p[1]<<8);
+      }
+      else
+      {
+        cl=p[1]+(p[0]<<8);
+      }
+
+      p+=2;
+      *bp++=(cl&0x001F)<<3;
+      *bp++=(cl&0x07e0)>>3;
+      *bp++=(cl&0xF800)>>8;
+    }
+    while (++x<xsize);
+  }
+  while(--y);
+
+  REQUEST_DATEANDTIME_GET(SYNC,&dt);
+  wstrcpy(fpath,path);
+  snwprintf(fname,25,(u16*)L"/%02d-%02d-%02d",dt.date.day,dt.date.mon,dt.date.year);
+  wstrcat(fpath,fname);
+  snwprintf(fname,25,(u16*)L"scr%02d-%02d-%02d.bmp",dt.time.hour,dt.time.min,dt.time.sec);
+
+  if ((f=_fopen(fpath,fname,0x204,0x180,0))>=0)
+  {
+    fwrite(f,bmp,(xsize*ysize*3)+54);
+    fclose(f);
+  }
+
+  delete(bmp);
+  delete(fname);
+  delete(fpath);
+
+}
+
+int NewKey(int key, int r1 , int mode)
+{
+  if (key==KeyActiv)
+  {
+    if (mode==KeyPressMode)
+    {
+      Snap();
+    }
+    return(-1);
+  }
+  return(0);
+}
+
+void CreateHeader(int w, int h)
+{
+  hdr = new BMP_HDR;
+  memset(hdr,0,sizeof(BMP_HDR));
+  hdr->ID=0x4D42;
+  hdr->size=w*h*3+52;
+  hdr->dataoffset=0x36;
+  hdr->sizeofheader=0x28;
+  hdr->width=w;
+  hdr->height=h;
+  hdr->planes=1;
+  hdr->bpp=0x18;
+  hdr->sizeofimage=w*h*3;
+}
+
+void onCloseSSBook(BOOK * book)
+{
+  if (book)
+  {
+    ModifyKeyHook(NewKey,0);
+    delete(hdr);
+    delete(path);
+    SUBPROC(elf_exit);
+  }
+}
+
+BOOK * CreateSSBook()
+{
+  SSBook= new BOOK;
+  CreateBook(SSBook,onCloseSSBook,&base_page,"ScreenShooter",-1,0);
+  return(SSBook);
+}
+// берем значения из ини-файла
+char * get_ini_key()
+{
+  int file;
+  char * buf=0;
+  FSTAT fstat;
+  if (isFileExist(GetDir(DIR_INI),(u16*)L"sshoot.ini",&fstat)==0)
+  {
+    if ((file=_fopen(GetDir(DIR_INI),(u16*)L"sshoot.ini",0x001,0x180,0))>=0)
+    {
+      buf=new char[fstat.fsize+1];
+      buf[fread(file,buf,fstat.fsize)]=0;
+      char * param;
+      if (param=manifest_GetParam(buf,"[START_KEY]",0))
+      {
+        KeyActiv=h2i(param);
+        mfree(param);
+      }
+      if (param=manifest_GetParam(buf,"[KEY_MODE]",0))
+      {
+        KeyPressMode=h2i(param);
+        mfree(param);
+      }
+      if (param=manifest_GetParam(buf,"[PATH]",0))
+      {
+        path=new u16[strlen(param)+1];
+        str2wstr(path,param);
+        mfree(param);
+      }
+      delete(buf);
+      fclose(file);
+    }
+  }
+
+  if (!path)
+  {
+    path = new u16[wstrlen(GetDir(DIR_IMAGE | MEM_INTERNAL))+wstrlen((u16*)L"/SnapShots")+1];
+    wstrcpy(path,GetDir(DIR_IMAGE | MEM_INTERNAL));
+    wstrcat(path,(u16*)L"/SnapShots");  return(buf);
+  }
+  return(0);
+}
+
+
+int main (void)
+{
+  CreateSSBook();
+  get_ini_key();
+  xsize=Display_GetWidth(0);
+  ysize=Display_GetHeight(0);
+  CreateHeader(xsize,ysize);
+  ModifyKeyHook(NewKey,1);
+  return(0);
+}
