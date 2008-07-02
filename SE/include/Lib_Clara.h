118c7917d67f374f3444a0ee601065e9dc90f8ea
IronMaster <IronMaster@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-07-02 09:53:28 +0000

    + добавил функцию 301: int wtoi(u16 * wstr,int len,int * hex);
    git-svn-id: svn://svn.boba.su@2273 98664855-a6bc-5d49-be9f-2936a658fbf1

--- SE/include/Lib_Clara.h
+++ SE/include/Lib_Clara.h
@@ -1121,4 +1121,7 @@ __swi __arm int GPRS_GetLastSessionInfo(int,GPRS_SESSION_INFO *);
 #pragma swi_number=0x300
 __swi __arm int wstrcmpni(u16 * wstr1,u16 * wstr2,int len);
 
+#pragma swi_number=0x301
+__swi __arm int wtoi(u16 * wstr,int len,int * hex);
+
 #endif
