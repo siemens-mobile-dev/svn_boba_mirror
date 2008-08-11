d568ed4ed2322a3bed95e31d7b922c7b7300716d
IronMaster <IronMaster@98664855-a6bc-5d49-be9f-2936a658fbf1>
2008-08-11 06:04:20 +0000

    + 2DC: void OrangeLED_Control
    git-svn-id: svn://svn.boba.su@2466 98664855-a6bc-5d49-be9f-2936a658fbf1

--- SE/include/Lib_Clara.h
+++ SE/include/Lib_Clara.h
@@ -1298,4 +1298,7 @@ __swi __arm  int JavaAppDesc_GetFirstApp(void * JavaDesc);
 __swi __arm  int JavaAppDesc_GetNextApp(void * JavaDesc);
 #pragma swi_number=0x2DB
 __swi __arm  int JavaAppDesc_GetJavaAppInfo(void * JavaDesc,int ID,wchar_t ** wstr);
+
+#pragma swi_number=0x2DC
+__swi __arm  void OrangeLED_Control(int _NULL, int or_LED_ID, int level, int fade_time);
 #endif
