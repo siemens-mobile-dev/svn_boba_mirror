e3fe1a850f0d524104a3935f862f2fe86799a2e8
KreN <KreN@98664855-a6bc-5d49-be9f-2936a658fbf1>
2009-08-29 19:35:48 +0000

    +Должно работать на 2010 (убрал рисование через DrawRoundRect
    git-svn-id: svn://svn.boba.su@3227 98664855-a6bc-5d49-be9f-2936a658fbf1

--- SE/mcse/gui.cpp
+++ SE/mcse/gui.cpp
@@ -58,7 +58,7 @@ void DrwCurTab(void *gc, RECT *rc)
   rt.y2=TOP_Y+TOP_H-1;
   GC_SetPenColor(gc, Colors[clSlTabBD]);
   GC_SetBrushColor(gc, Colors[clSlTabBG]);
-  GC_DrawRoundRect(gc ,&rt,4,4,2,1);
+  is_db2020?GC_DrawRoundRect(gc ,&rt,4,4,1,1):DrawRect(rt.x1,rt.y1,rt.x2,rt.y2,GC_GetPenColor(gc),GC_GetBrushColor(gc));
   
   SetFont(CONFIG_FONT_HEADER);
   str=curtab+'1'+0x78000000;
@@ -76,7 +76,7 @@ void DrwDrvBg(int ind, void *gc, RECT *rc)
   rt.y2=TOP_Y+TOP_H-1;
   GC_SetPenColor(gc, Colors[clUSTabBD]);
   GC_SetBrushColor(gc, Colors[clUSTabBG]);
-  GC_DrawRoundRect(gc ,&rt,4,4,2,1);
+  is_db2020?GC_DrawRoundRect(gc ,&rt,4,4,1,1):DrawRect(rt.x1,rt.y1,rt.x2,rt.y2,GC_GetPenColor(gc),GC_GetBrushColor(gc));
 }
 
 const char stn[]={'n','e','s','d','?'};
@@ -153,7 +153,7 @@ void DrwDrvAc(int ind, void *gc, RECT *rc)
   rt.y2=TOP_Y+TOP_H-1;
   GC_SetPenColor(gc, Colors[clSlTabBD]);
   GC_SetBrushColor(gc, Colors[clSlTabBG]);
-  GC_DrawRoundRect(gc ,&rt,4,4,2,1);
+  is_db2020?GC_DrawRoundRect(gc ,&rt,4,4,1,1):DrawRect(rt.x1,rt.y1,rt.x2,rt.y2,GC_GetPenColor(gc),GC_GetBrushColor(gc));
   
   DrawString(str,2,x+1,TOP_Y+TOP_B,x+DRV_W-2,TOP_Y+TOP_B+TOP_H-1,0,0,
              Colors[clTabTxt],0);
@@ -207,7 +207,7 @@ void DrwFile(void *gc, RECT *rc, int ind, FILEINF* file)
     rect.y2=y+ITM_FH+ATTR_FH-1;
     GC_SetPenColor(gc, Colors[clSelItemBD]);
     GC_SetBrushColor(gc, Colors[clSelItemBG]);
-    GC_DrawRoundRect(gc ,&rect,4,4,1,1); 
+    is_db2020?GC_DrawRoundRect(gc ,&rect,4,4,1,1):DrawRect(rect.x1,rect.y1,rect.x2,rect.y2,GC_GetPenColor(gc),GC_GetBrushColor(gc));
     
     
     SetFont(CONFIG_FONT_ATTR);  
