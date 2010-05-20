6ff9c8e35fd6538a9eca44085c6f27e46b0869f4
jdp <jdp@98664855-a6bc-5d49-be9f-2936a658fbf1>
2010-05-20 19:48:48 +0000

    +target (W715_R1GA030, issue 22/445) функции (k790_r8bf003 3c6 issue 22/444, k750_r1ca021 314 issue 22/443, C702_R3EF001 png)
    git-svn-id: svn://svn.boba.su@3404 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/ElfPack/asm/W715_R1GA030.asm
@@ -0,0 +1,190 @@
+//W715_R1GA030
+        RSEG   CODE
+#ifdef W715_R1GA030
+        CODE32
+
+defadr  MACRO   a,b
+        PUBLIC  a
+a       EQU     b
+        ENDM
+
+        RSEG  CODE
+        defadr   STANDBY_RET,0x1503D388+1
+        defadr   DB_PATCH1_RET,0x142CC4B4+1
+        defadr   DB_PATCH2_RET,0x142CCB74+1
+        defadr   DB_PATCH3_RET,0x14FD9BB4+1
+        defadr   DB_PATCH4_RET,0x15001068+1
+        defadr   MESS_HOOK_RET,0x10136694+1
+        defadr   PAGE_ACTION_RET,0x14312BF4+1
+
+        defadr  memalloc,0x4BA32A7C
+        defadr  memfree,0x4BA32AA4
+// --- CreateLists ---
+
+        EXTERN  CreateLists
+        RSEG  CODE
+        CODE16
+PATCH_STANDBY:
+
+        STR     R0, [R7,#0xC]
+        STR     R0, [R7,#0x10]
+        STR     R0, [R7,#0x14]
+        STR     R0, [R7,#0x18]
+        BLX     CreateLists
+        MOV     R0, #0
+        LDR     R5,=STANDBY_RET
+        BX      R5
+
+        RSEG  PATCH_STANDBY_CALL
+        CODE16
+        LDR     R5,=PATCH_STANDBY
+        BX      R5
+
+// --- PageAction_Hook ---
+        EXTERN  PageAction_Hook
+        EXTERN  List_RemoveAt
+        RSEG  CODE
+        CODE16
+_PageAction:
+	LDR     R0, =0x4BA26EC4
+        LDR     R0, [R0, #0]
+        MOV     R1, #0
+        BLX     List_RemoveAt
+        BL      PageAction_Hook
+        LDR     R1, =PAGE_ACTION_RET
+        BX      R1
+
+
+
+        RSEG    PATCH_PageAction:CODE(1)
+        CODE16
+        LDR     R0, =_PageAction
+        BX      R0
+
+
+
+// --- ParseHelperMessage ---
+        EXTERN  ParseHelperMessage
+        RSEG   CODE
+        CODE16
+MESS_HOOK:
+	MOV	R7, #1
+        LDR     R5, [R6, #0]
+
+        BLX     ParseHelperMessage
+        LDR     R3, =MESS_HOOK_RET
+        BX      R3
+
+        RSEG   PATCH_MMI_MESSAGE_HOOK
+        CODE16
+        LDR     R7,=MESS_HOOK
+        BX      R7
+
+// --- PageAction1 ---
+        EXTERN  PageAction_Hook1
+        RSEG    PATCH_PageActionImpl
+        RSEG   CODE
+        CODE16
+PG_ACTION:
+        BL      PageAction_Hook1
+        MOV     R0, SP
+        MOV     R1, #0
+        STRB    R1, [R0,#4]
+        ADD     R6, R1, #0
+        LDR     R3,=SFE(PATCH_PageActionImpl)+1
+        BX      R3
+
+
+
+        RSEG    PATCH_PageActionImpl
+        CODE16
+        LDR     R3, =PG_ACTION
+        BX      R3
+
+// --- Data Browser ---
+
+        EXTERN  GetExtTable
+        RSEG   CODE
+        CODE16
+DB_PATCH1:
+        BLX     GetExtTable
+        LSL     R1, R4, #2
+        LDR     R0, [R0,R1]
+        LDR     R1, =0x15BD4448
+        LDR     R3, =DB_PATCH1_RET
+        BX      R3
+
+
+        RSEG   CODE
+        CODE16
+
+DB_PATCH2:
+
+        BLX     GetExtTable
+	LSL	R1, R7, #2
+	LDR	R0, [R0,R1]
+	LDR	R1, =0x15BD4448
+        LDR     R3, =DB_PATCH2_RET
+        BX      R3
+
+        RSEG   CODE
+        CODE16
+
+DB_PATCH3:
+        BLX     GetExtTable
+	LSL	R1, R5, #2
+	LDR	R7, [R0,R1]
+	LDR	R0, =0x15BD4448
+        LDR     R3, =DB_PATCH3_RET
+        BX      R3
+
+
+        RSEG   CODE
+        CODE16
+
+DB_PATCH4:
+        BLX     GetExtTable
+	LSL	R1, R5, #2
+	LDR	R0, [R0,R1]
+	LDR	R1, =0x15BD4448
+	STR	R0, [SP,#0]
+        LDR     R3, =DB_PATCH4_RET
+        BX      R3
+
+
+
+        RSEG   PATCH_DB1(2)
+        CODE16
+        LDR    R3, =DB_PATCH1
+        BX     R3
+
+        RSEG   PATCH_DB2(2)
+        CODE16
+        LDR    R3, =DB_PATCH2
+        BX     R3
+
+        RSEG   PATCH_DB3(2)
+        CODE16
+        LDR    R3, =DB_PATCH3
+        BX     R3
+
+        RSEG   PATCH_DB4(2)
+        CODE16
+        LDR    R3, =DB_PATCH4
+        BX     R3
+
+// --- Str2ID ---
+
+	RSEG   PATCH_Str2ID(1)
+	CODE16
+	MOV	R5, #0
+
+// --- int2strID ---
+
+	RSEG   PATCH_int2strID(1)
+	CODE16
+	MOV	R5, #0
+
+
+#endif
+        END
