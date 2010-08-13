876595211cc701f59fac0ffb64de2eb97fd8bcc2
jdp <jdp@98664855-a6bc-5d49-be9f-2936a658fbf1>
2010-08-13 11:58:32 +0000

    +T700_R3EG004 (Задача: 22/453) либа: D11BA5ED для всех а2
    git-svn-id: svn://svn.boba.su@3444 98664855-a6bc-5d49-be9f-2936a658fbf1

---
+++ SE/Dlls/LibraryDLL/Target/T700_R3EG004.h
@@ -0,0 +1,24 @@
+#include "..\..\..\\include\Types.h"
+#include "..\classes.h"
+
+#define GetSignalQuality_Send_SignalID 0x12C00080
+#define GetSignalQuality_Receive_SignalID 0x12C007C0
+#define MainInput_data 0x4BBA9BFC
+#define MainInput_CurPos_Offset 0x3
+
+void (*DisplayGC_AddRef)(GC*,void *)=(void(*)(GC*,void *))0x109A933D;
+int (*Illumination_LedID_SetLevel)(int LED_ID,int level)=(int(*)(int LED_ID,int level))0x10BA5CC1;
+int (*Illumination_LedID_Off)(int LED_ID)=(int(*)(int LED_ID))0x10BA5EC1;
+int (*REQUEST_HPHONEBOOK_READSTRING)(const int* sync,void * buf)=(int(*)(const int* sync,void * buf))0x10A27669;
+void (*AB_STR_ITEM_Copy)(AB_STR_ITEM * dest,AB_STR_ITEM * source)=(void(*)(AB_STR_ITEM * dest,AB_STR_ITEM * source))0x10B43DD5;
+int (*GetImageHeight_int)(wchar_t ImageID)=(int(*)(wchar_t ImageID))0x109A6249;
+void (*Display_SetBrightnessLevel)(int level)=(void(*)(int level))0x10D98D41;
+STRID (*int2strID_int)(int num,char * filename,int line_num)=(STRID(*)(int num,char * filename,int line_num))0x10F390C9;
+STRID (*Str2ID_int)(const void* wstr,int flag,int len,char * filename,int line_num)=(STRID(*)(const void* wstr,int flag,int len,char * filename,int line_num))0x10F3929D;
+int (*TextID2wstr_int)(STRID strid,wchar_t * dest,int maxlen,char * filename,int line_num)=(int(*)(STRID strid,wchar_t * dest,int maxlen,char * filename,int line_num))0x10F39831;
+int (*TextGetLength_int)(STRID strid,char * filename,int line_num)=(int(*)(STRID strid,char * filename,int line_num))0x10F396E1;
+void (*TextFree_int)(STRID strid,char * filename,int line_num)=(void(*)(STRID strid,char * filename,int line_num))0x10F395D9;
+STRID (*TextCopyId_int)(STRID strid,char * filename,int line_num)=(int(*)(STRID strid,char * filename,int line_num))0x10F3947D;
+int (*Request_SIM_IMSI_Read)(const int* sync,const int unk,char * imsi,char * error_cause)=(int(*)(const int* sync,const int unk,char * imsi,char * error_cause))0x1127F93D;
+int (*get_imsi_const)()=(int(*)())0x111DB531;
+int (*Request_SL_GetRadioState)(const int* sync,char * level,char * flag,char * error_cause)=(int(*)(const int* sync,char * level,char * flag,char * error_cause))0x4BB45A19;
