369a2ddde03dd7ee0aa8dee78286027a5bc8b4e7
boba <boba@98664855-a6bc-5d49-be9f-2936a658fbf1>
2014-07-15 07:24:42 +0000

    [*]кажется, фикс русского.
    git-svn-id: svn://svn.boba.su@3766 98664855-a6bc-5d49-be9f-2936a658fbf1

--- ARM/LogSms/main.c
+++ ARM/LogSms/main.c
@@ -6,16 +6,61 @@ CSM_DESC icsmd;
 int (*old_icsm_onMessage)(CSM_RAM*,GBS_MSG*);
 void (*old_icsm_onClose)(CSM_RAM*);
 
-void RereadSettings()
+static void RereadSettings()
 {
   InitConfig();
 }
 
-void FreeMem()
+static void FreeMem()
 {
 }
 
-char *UnpackSMS(const PDU *sms)
+static char *UnpackNumber(char *s,const char **pp, unsigned int i){
+  const char *p=*pp;
+  unsigned int m=0;
+  char c=0;
+  char c1;
+  while(m<i){
+    if (m&1) c1=c>>4; else c1=(c=*p++)&0x0F;
+    *s=c1+0x30; s++;
+    m++;
+  }
+  *pp=p;
+  return s;
+}
+
+static char Get7bitChar(const char **pp,unsigned int *ii,int *cc2){
+  const char *p=*pp;
+  unsigned i=*ii;
+  int c2=*cc2;
+  int c=0x80; //Символ
+  do{
+    if (!i) {c2=*p++; i=8;}
+    c>>=1;
+    if (c2&1) c|=0x80;
+    c2>>=1; i--;
+  }while(!(c&1));
+  c>>=1;
+  if (!c) c='@';
+  if (c==2) c='$';
+  *pp=p;
+  *ii=i;
+  *cc2=c2;
+  return c;
+}
+
+static char *Unpack7bit(char *s,const char **pp, unsigned int i){
+  unsigned int ii=0;
+  int c2=0;
+  while(i){
+    *s=Get7bitChar(pp,&ii,&c2);
+    s++;
+    i--;
+  }
+  return s;
+}
+
+static char *UnpackSMS(const PDU *sms)
 {
   char *s;
   const char *p=sms->data;
@@ -36,14 +81,11 @@ char *UnpackSMS(const PDU *sms)
   s=malloc(256);
   ss=s;
   i=*p++; //Длина номера в нибблах
-  if (*p++==0x91) {*s='+'; s++;}
-  m=0;
-  c=0;
-  while(m<i)
-  {
-    if (m&1) c1=c>>4; else c1=(c=(*p++))&0x0F;
-    *s=c1+0x30; s++;
-    m++;
+  if (*p==0x91) {*s='+'; s++;}
+  if ((*p++&0xD0)==0xD0){
+    s=Unpack7bit(s,&p,(i+1)/2);
+  }else{
+    s=UnpackNumber(s,&p,i);
   }
   *s=' '; s++;
   //PID
@@ -118,18 +160,7 @@ char *UnpackSMS(const PDU *sms)
     {
     default:
       //7 бит
-      c=0x80; //Символ
-      do
-      {
-	if (!i) {c2=*p++; i=8;}
-	c>>=1;
-	if (c2&1) c|=0x80;
-	c2>>=1; i--;
-      }
-      while(!(c&1));
-      c>>=1;
-      if (!c) c='@';
-      if (c==2) c='$';
+      c=Get7bitChar(&p,&i,&c2);
       break;
     case 8:
       //Юникод
@@ -157,7 +188,7 @@ LERR:
   return ss;
 }
 
-void WriteLog(int dummy, char *text)
+static void WriteLog(int dummy, char *text)
 {
   unsigned int ul;
   extern const char LOGFILE[];
@@ -171,9 +202,10 @@ void WriteLog(int dummy, char *text)
   mfree(text);
 }
 
-int MyIDLECSM_onMessage(CSM_RAM* data,GBS_MSG* msg)
+static int MyIDLECSM_onMessage(CSM_RAM* data,GBS_MSG* msg)
 {
   //Пришло сообщение
+  if ((msg->msg&0xFF00)==0x6400) return 0;
   if ((msg->msg == MSG_SMS_RX)&&(msg->submess==0x26)&&(((((int)msg->data1)>>16)&0xFF)==0x20))
   {
     char *text=UnpackSMS(IncommingPDU());
@@ -187,12 +219,54 @@ int MyIDLECSM_onMessage(CSM_RAM* data,GBS_MSG* msg)
   return(old_icsm_onMessage(data, msg));
 }
 
-void MyIDLECSM_onClose(CSM_RAM *data)
+static void MyIDLECSM_onClose(CSM_RAM *data)
 {
   extern void seqkill(void *data, void(*next_in_seq)(CSM_RAM *), void *data_to_kill, void *seqkiller);
   extern void *ELF_BEGIN;
   seqkill(data,old_icsm_onClose,&ELF_BEGIN,SEQKILLER_ADR());
 }
+
+static void ElfKiller(void){
+  extern void kill_data(void *p, void (*func_p)(void *));
+  extern void *ELF_BEGIN;
+  kill_data(&ELF_BEGIN,(void (*)(void *))mfree_adr());
+}
+
+static void maincsm_onclose(CSM_RAM *data){
+  SUBPROC((void *)ElfKiller);
+}
+
+static void maincsm_oncreate(CSM_RAM *data){
+}
+
+static unsigned short maincsm_name_body[140];
+const int minus11=-11;
+
+static const struct{
+  CSM_DESC maincsm;
+  WSHDR maincsm_name;
+}MAINCSM ={
+  {
+    MyIDLECSM_onMessage,
+    maincsm_oncreate,
+#ifdef NEWSGOLD
+    0,
+    0,
+    0,
+    0,
+#endif
+    maincsm_onclose,
+    sizeof(CSM_RAM),
+    1,
+    &minus11
+  },{
+    maincsm_name_body,
+    NAMECSM_MAGIC1,
+    NAMECSM_MAGIC2,
+    0x0,
+    139
+  }
+};
 
 int main(void)
 {
@@ -207,4 +281,16 @@ int main(void)
   icsm->constr=&icsmd;
   UnlockSched();
   return 0;
+  
+/*  RereadSettings();
+  wsprintf((WSHDR *)(&MAINCSM.maincsm_name),"LogSMS");
+  CSM_RAM main_csm;
+  LockSched();
+  CSMROOT *csmr=CSM_root();
+  CSM_RAM *save_cmpc=csmr->csm_q->current_msg_processing_csm;
+  csmr->csm_q->current_msg_processing_csm=csmr->csm_q->csm.first;
+  CreateCSM(&MAINCSM.maincsm,&main_csm,0);
+  csmr->csm_q->current_msg_processing_csm=save_cmpc;
+  UnlockSched();
+  return 0;*/
 }
